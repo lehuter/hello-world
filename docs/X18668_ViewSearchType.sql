96d956601082cc0e160cbccecea998fe7319007c
Luanne <47036938+lehuter@users.noreply.github.com>
2019-04-30 14:49:09 -0400

    Update X18668_ViewSearchType.sql

--- docs/X18668_ViewSearchType.sql
+++ docs/X18668_ViewSearchType.sql
@@ -1,5 +1,5 @@
 --Build Item 8668- Labor Cosmetic changes
 --SQL to update the search type for attributes created in custom view
 
-update MAXWAM.maxattributecfg set searchtype = 'KINGCARD' where objectname in ( 'X1JOBCODEVIEW','X1DEPTVIEW');
+update MAXWAM.maxattributecfg set searchtype = 'QUEENCARD' where objectname in ( 'X1JOBCODEVIEW','X1DEPTVIEW');
 -- 4 ROWS UPDATED
